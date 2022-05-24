30826260362e184e0dfbbcd47a85490c6a0171cf
gabrielraymond <gabrielraymond.grd@gmail.com>
2022-05-24 14:54:00 +0700

    change index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
@@ -14,4 +13,3 @@ app.get("/*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 5000);
-
